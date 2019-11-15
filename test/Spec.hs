b6fde61d201256bfef7ac52a0e0d1fec04af8292
jasonincanada <nirgle@gmail.com>
2019-11-15 11:41:29 -0500

    set up tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,22 @@
+import Test.Hspec
+import Control.Monad (forM_)
+
+import MarblesTree (marblestree)
+
+path = "test/inputs/"
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+
+  describe "MarblesTree" $ do
+
+    let cases = [ ( "marblestree-1.input", 7  )
+                , ( "marblestree-2.input", 14 )
+                , ( "marblestree-3.input", 20 )
+                ]
+
+    forM_ cases $
+      \(testfile, result) ->
+        it testfile $ (marblestree <$> readFile (path ++ testfile))
+                        >>= (`shouldBe` (unlines [show result]))
+
