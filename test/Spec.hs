57fbb3321ab83a68438af0fa9a8f5f515dafd980
jasonincanada <nirgle@gmail.com>
2019-11-15 12:46:21 -0500

    add tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -10,9 +10,25 @@ main = hspec $ do
 
   describe "MarblesTree" $ do
 
-    let cases = [ ( "marblestree-1.input", 7  )
+    let cases = [
+                -- Sample inputs from the problem page
+                  ( "marblestree-1.input", 7  )
                 , ( "marblestree-2.input", 14 )
                 , ( "marblestree-3.input", 20 )
+
+                -- Probing inputs searching for the bug
+                , ( "marblestree-4.input",  0 )
+                , ( "marblestree-5.input",  3 )
+                , ( "marblestree-6.input",  0 )
+                , ( "marblestree-7.input",  1 )
+                , ( "marblestree-8.input",  3 )
+                , ( "marblestree-9.input",  3 )
+                , ( "marblestree-10.input", 2 )
+                , ( "marblestree-11.input", 6 )
+                , ( "marblestree-12.input", 3 )
+                , ( "marblestree-13.input", 5 )
+                , ( "marblestree-14.input", 7 )
+                , ( "marblestree-15.input", 4 )
                 ]
 
     forM_ cases $
