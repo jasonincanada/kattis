e56a2005264a32a3108458c3ef43d00380791576
Jason Hooper <nirgle@gmail.com>
2022-06-14 10:41:44 -0400

    rust code adds the index

--- rust/romanholidays/generate_vecs.sh
+++ rust/romanholidays/generate_vecs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
-# generates the static maps of roman numbers and their offsets into their respective sorted lists
+# generates the static maps of roman numbers. the rust code adds their offset into the sorted list
+# when it converts the vec into a map, using enumerate()
 
 generate() {
     NAME="$1"
